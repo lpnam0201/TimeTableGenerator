3fb3d50bf9db1be12e393ff62a510f162f354a46
NamLP <lpnam0201@gmail.com>
2025-07-29 23:49:14 +0700

    site works

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,19 +1,174 @@
 $(document).ready(function() {
-    initializeComboboxes();
-    loadJsonDataToStorage();
+    loadJsonDataToStorage()
+        .then((items) => populateYearCombobox(items));
 });
 const UlawAutomationDataKey = 'UlawAutomationData';
+const SelectStudentYearComboboxId = "#select-student-year-combobox";
+const SelectStudentSemesterComboboxId = "#select-student-semester-combobox";
+const SelectStudentCourseComboboxId = "#select-student-course-combobox";
+const SelectStudentClassComboboxId = "#select-student-class-combobox";
+const ResultTextboxId = "#result-textbox";
+ 
+function populateYearCombobox(items) {
+    let idCounter = 1;
+    let yearItems = [];
+    let years = Array.from(new Set(items.map(x => x.year)));
 
-function initializeComboboxes() {
-    let comboboxClasses = [
-        'select-student-year',
-        'select-student-term',
-        'select-student-course',
-        'select-student-class',
-    ];
-    for (let comboboxClass of comboboxClasses) {
-        $(comboboxClass).select2();
+    for (let year of years) {
+        yearItems.push({
+            id: idCounter,
+            text: year
+        });
+        
+        idCounter++;
     }
+
+    $(SelectStudentYearComboboxId)
+        .select2({
+            data: yearItems
+        })
+        .on('select2:select', (e) => {
+            onYearComboboxSelected();
+        });
+}
+
+
+function populateSemesterCombobox(selectedYear) {
+    let items = JSON.parse(localStorage.getItem(UlawAutomationDataKey));
+    items = items.filter(x => x.year === selectedYear);
+    let idCounter = 1;
+    let semesterItems = [];
+    let semesters = Array.from(new Set(items.map(x => x.semester)));
+
+    for (let semester of semesters) {
+        semesterItems.push({
+            id: idCounter,
+            text: semester
+        });
+        
+        idCounter++;
+    }
+
+    $(SelectStudentSemesterComboboxId)
+        .select2({
+            data: semesterItems
+        })
+        .on('select2:select', (e) => {
+            onSemesterComboboxSelected();
+        });
+}
+
+function clearSemesterCombobox() {
+    $(SelectStudentSemesterComboboxId).empty().append(new Option());
+    $(SelectStudentSemesterComboboxId).val(null);
+}
+
+function populateCourseCombobox(selectedYear, selectedSemester) {
+    let items = JSON.parse(localStorage.getItem(UlawAutomationDataKey));
+    items = items.filter(x => x.year === selectedYear && x.semester === selectedSemester);
+    let idCounter = 1;
+    let courseItems = [];
+    let courses = Array.from(new Set(items.map(x => x.course)));
+
+    for (let course of courses) {
+        courseItems.push({
+            id: idCounter,
+            text: course
+        });
+        
+        idCounter++;
+    }
+
+    $(SelectStudentCourseComboboxId)
+        .select2({
+            data: courseItems
+        })
+        .on('select2:select', (e) => {
+            onCourseComboboxSelected();
+        });
+}
+
+function clearCourseCombobox() {
+    $(SelectStudentCourseComboboxId).empty().append(new Option());
+    $(SelectStudentCourseComboboxId).val(null);
+}
+
+function populateClassCombobox(selectedYear, selectedSemester, selectedCourse) {
+    let items = JSON.parse(localStorage.getItem(UlawAutomationDataKey));
+    items = items.filter(x => x.year === selectedYear && x.semester === selectedSemester && x.course === selectedCourse);
+    let idCounter = 1;
+    let classItems = [];
+    let classes = Array.from(new Set(items.map(x => x.fileName)));
+
+    for (let clazz of classes) {
+        classItems.push({
+            id: idCounter,
+            text: clazz
+        });
+    
+        idCounter++;
+    }
+
+    $(SelectStudentClassComboboxId)
+        .select2({
+            data: classItems
+        })
+        .on('select2:select', (e) => {
+            onClassComboboxSelected();
+        });
+}
+
+function clearClassCombobox() {
+    $(SelectStudentClassComboboxId).empty().append(new Option());
+    $(SelectStudentClassComboboxId).val(null);
+}
+
+function onYearComboboxSelected() {
+    clearSemesterCombobox();
+    clearCourseCombobox();
+    clearClassCombobox();
+    clearResultBox();
+    const selectedYear = $(SelectStudentYearComboboxId).select2('data')[0].text;
+    populateSemesterCombobox(selectedYear);
+}
+
+function onSemesterComboboxSelected() {
+    clearCourseCombobox();
+    clearClassCombobox();
+    clearResultBox();
+    const selectedYear = $(SelectStudentYearComboboxId).select2('data')[0].text;
+    const selectedSemester = $(SelectStudentSemesterComboboxId).select2('data')[0].text;
+    populateCourseCombobox(selectedYear, selectedSemester);
+}
+
+function onCourseComboboxSelected() {
+    clearClassCombobox();
+    clearResultBox();
+    const selectedYear = $(SelectStudentYearComboboxId).select2('data')[0].text;
+    const selectedSemester = $(SelectStudentSemesterComboboxId).select2('data')[0].text;
+    const selectedCourse = $(SelectStudentCourseComboboxId).select2('data')[0].text;
+    populateClassCombobox(selectedYear, selectedSemester, selectedCourse);
+}
+
+function onClassComboboxSelected() {
+    const selectedYear = $(SelectStudentYearComboboxId).select2('data')[0].text;
+    const selectedSemester = $(SelectStudentSemesterComboboxId).select2('data')[0].text;
+    const selectedCourse = $(SelectStudentCourseComboboxId).select2('data')[0].text;
+    const selectedClass = $(SelectStudentClassComboboxId).select2('data')[0].text;
+    fillResult(selectedYear, selectedSemester, selectedCourse, selectedClass);
+}
+
+function fillResult(selectedYear, selectedSemester, selectedCourse, selectedClass) {
+    let items = JSON.parse(localStorage.getItem(UlawAutomationDataKey));
+    let item = items.find(x => x.year === selectedYear 
+        && x.semester === selectedSemester 
+        && x.course === selectedCourse
+        && x.fileName === selectedClass);
+    $(ResultTextboxId).val(item.fileUrl);
+}
+
+function clearResultBox() {
+    $(ResultTextboxId).val(null);
 }
 
 function loadJsonDataToStorage() {
@@ -32,11 +187,12 @@ function loadJsonDataToStorage() {
         loadDataPromises.push(promise);
     }
     let resolved = Promise.all(loadDataPromises);
-    resolved.then((responses) => {
+    return resolved.then((responses) => {
         let items = [];
         for (let response of responses) {
             items.push(...response);
         }
         localStorage.setItem(UlawAutomationDataKey, JSON.stringify(items));
+        return items;
     })
 }
